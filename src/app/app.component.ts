80a44427593ce3e8f6c6e14a4e99287bc77c7fe5
Marivaldo Pereira de Oliveira Junior <marivaldo.junior@serpro.gov.br>
2020-03-13 14:54:01 -0300

    Algumas pequenas alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,21 @@ import { Hero } from './hero';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero.name}}</h2>
-  <p>Heroes:</p>
-  <ul>
-    <li *ngFor="let hero of heroes">
-      {{ hero.name }}
-      </li>
-  </ul>
-  <p *ngIf="heroes.length > 3">There are many heroes!</p>
+  <input #newHero
+    (keyup.enter)="addHero(newHero.value)"
+    (blur)="addHero(newHero.value); newHero.value='' ">
+
+  <button (click)="addHero(newHero.value)">Add</button>
+
+  <ul><li *ngFor="let hero of heroes">{{hero}}</li></ul>
 `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = [
-    new Hero(1, 'Windstorm'),
-    new Hero(13, 'Bombasto'),
-    new Hero(15, 'Magneta'),
-    new Hero(20, 'Tornado')
-  ];
-  myHero = this.heroes[0];
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  addHero(newHero: string) {
+    if (newHero) {
+      this.heroes.push(newHero);
+    }
+  }
 }
