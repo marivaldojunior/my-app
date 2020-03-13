41c50445a264016bbf26fab660ff9946b53eed16
Marivaldo Pereira de Oliveira Junior <marivaldo.junior@serpro.gov.br>
2020-03-13 15:16:20 -0300

    Algumas pequenas alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <input #newHero
-    (keyup.enter)="addHero(newHero.value)"
-    (blur)="addHero(newHero.value); newHero.value='' ">
-
-  <button (click)="addHero(newHero.value)">Add</button>
-
-  <ul><li *ngFor="let hero of heroes">{{hero}}</li></ul>
-`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  addHero(newHero: string) {
-    if (newHero) {
-      this.heroes.push(newHero);
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me/click-me.component';
+import { KeyupComponent } from './keyup/keyup.component';
+import { LoopBackComponent } from './loop-back/loop-back.component';
+import { LittleTourComponent } from './little-tour/little-tour.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickMeComponent,
+    KeyupComponent,
+    LoopBackComponent,
+    LittleTourComponent
   ],
   bootstrap: [ AppComponent ]
 })
